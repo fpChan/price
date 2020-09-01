61969d694b1c9a90df9d5ec7e8a98122fb871595
fpChan <fpchen@outlook.com>
2020-09-01 23:10:36 +0800

    format && add tx_hash as params

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use reqwest::Error;
 
 use ckb_sdk::{
     constants::{SIGHASH_TYPE_HASH},
-    wallet::DerivationPath, HttpRpcClient, SignerFn, TxHelper,GenesisInfo,
+    HttpRpcClient,
 };
 use ckb_types::core::BlockView;
 
@@ -23,12 +23,27 @@ use ckb_hash::{blake2b_256, new_blake2b};
 
 
 use faster_hex::{hex_decode};
-use ckb_types::packed::{CellDep, OutPoint};
+use ckb_types::packed::{CellDep, OutPoint, CellOutput, Byte32};
 use ckb_types::core::DepType;
 
 
 const SIGNATURE_SIZE: usize = 65;
-const CELL_CAPACITY: u64 = 16 * 100000000 + 14100000000;
+const CELL_CAPACITY: u64 = 16 * 100000000 + 14000000000;
+/*
+account config
+  address:
+    mainnet: ckb1qyqymh2axts7azldsvmqltp0wzcr5mjr0zeqqyf57g
+    testnet: ckt1qyqymh2axts7azldsvmqltp0wzcr5mjr0zeqaphtj5
+  has_ckb_root: true
+  lock_arg: 0x4ddd5d32e1ee8bed83360fac2f70b03a6e4378b2
+  lock_hash: 0xcc112ffd6128d98ee1b10f4eb9c08e3271c616d3bf2e2e1cb67ca9af94583ad8
+  source: Local File System
+
+  export private key and chain code
+  0b008309faea434378cdfcdb36f3c21e477406af29d224edfcb3576ce348f56c
+  f0006f214ff8fc6266fe7fbf7baceef4f7e4116df426853263a6633493ea93ef
+ */
+
 
 #[derive(Deserialize, Debug)]
 struct Data {
@@ -42,31 +57,39 @@ struct Res {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let request_url = String::from("https://widgets.coinmarketcap.com/v2/ticker/4948/");
+    let mut input_hash = "0x6f2321befe738192959de2f64e6827b2c3afc978ae4889ce5383776fe87f4cae";
 
     // get price
-    let request_url = String::from("https://widgets.coinmarketcap.com/v2/ticker/4948/");
     let response: Res = reqwest::get(&request_url).await?.json().await?;
     let price = response.data.quotes.get("USD").unwrap().get("price").unwrap();
     println!("price: {:?}", price);
+
     //  CKB RPC client
     let mut rpc_client = HttpRpcClient::new(String::from("http://127.0.0.1:8114"));
 
-    let tx = get_tx(price);
+    let tx = get_tx(price, input_hash.to_owned().clone());
     let tx_hash = rpc_client.send_transaction(tx).map_err(|err| format!("Send transaction error: {}", err)).unwrap();
-    println!("hash result : {:?}",tx_hash.to_string());
+    println!("hash result 1 : {:?}", tx_hash.to_string());
+
 
     Ok(())
 }
 
-pub fn get_tx(price:&f64) -> packed::Transaction {
-
+pub fn get_tx(price: &f64, input_tx_hash: String) -> packed::Transaction {
     let mut ckb_client = HttpRpcClient::new(String::from("http://127.0.0.1:8114"));
 
-    // input tx hash
-    let input_tx_hash = h256!("0xcd866808ba9f51a0363f424d217f9fc842b3d95c14460880e3e41b41642bd327");
+
+    println!("the parpms : {}", input_tx_hash.clone());
+    // let input_tx_hash = h256!("0x6f2321befe738192959de2f64e6827b2c3afc978ae4889ce5383776fe87f4cae");
+    let tx_hash = input_tx_hash.clone();
+
+    let mut tx_hash_bytes = [0u8; 32];
+    hex_decode(&tx_hash.as_bytes(), &mut tx_hash_bytes);
+
 
     let cell_input = packed::OutPoint::new_builder()
-        .tx_hash(input_tx_hash.pack())
+        .tx_hash(H256::from(tx_hash_bytes).pack())
         .index(0u32.pack())
         .build();
 
@@ -76,8 +99,7 @@ pub fn get_tx(price:&f64) -> packed::Transaction {
         .build();
     let mut inputs = vec![input];
 
-    let lock_args = gen_lock_args(h256!("0x4ddd5d32e1ee8bed83360fac2f70b03a6e4378b2"));
-    let lock_script = gen_lockscript(lock_args);
+    let lock_script = gen_lockscript();
 
     // cell output
     let output = packed::CellOutput::new_builder()
@@ -89,18 +111,19 @@ pub fn get_tx(price:&f64) -> packed::Transaction {
     // outputs_data: the price of ckb
     let mut outputs_data: Vec<Bytes> = vec![Bytes::from(price.to_string())];
 
-    let block: BlockView = ckb_client
-        .get_block_by_number(0)
-        .expect("get genesis block failed from ckb")
-        .expect("genesis block is none")
-        .into();
+    // let block: BlockView = ckb_client
+    //     .get_block_by_number(0)
+    //     .expect("get genesis block failed from ckb")
+    //     .expect("genesis block is none")
+    //     .into();
+    // let ckb_genesis_info =
+    //     GenesisInfo::from_block(&block).expect("ckb genesisInfo generated failed");
+    // let secp256_dep: packed::CellDep =  ckb_genesis_info.sighash_dep();
 
-    let ckb_genesis_info =
-        GenesisInfo::from_block(&block).expect("ckb genesisInfo generated failed");
-
-
-    let secp256_dep: packed::CellDep =  ckb_genesis_info.sighash_dep();
-
+    let secp256_dep: packed::CellDep = CellDep::new_builder()
+        .out_point(cell_input.clone())
+        .dep_type(DepType::DepGroup.into())
+        .build();
     // build transaction
     let tx = TransactionBuilder::default()
         .inputs(inputs)
@@ -109,7 +132,7 @@ pub fn get_tx(price:&f64) -> packed::Transaction {
         .cell_dep(secp256_dep)
         .build();
 
-    // import private key for Privkey
+    // import account private key for Privkey
     let private_key = "0b008309faea434378cdfcdb36f3c21e477406af29d224edfcb3576ce348f56c";
     let mut privkey_bytes = [0u8; 32];
     hex_decode(private_key.as_bytes(), &mut privkey_bytes);
@@ -117,10 +140,10 @@ pub fn get_tx(price:&f64) -> packed::Transaction {
     // sign
     let privkey = ckb_crypto::secp::Privkey::from_slice(&privkey_bytes);
     let tx = sign_tx(tx, &privkey);
-    println!("data sign tx:  {}",tx.clone() );
 
     tx.data()
 }
+
 
 pub fn sign_tx(tx: ckb_types::core::TransactionView, key: &Privkey) -> TransactionView {
     let witnesses_len = tx.witnesses().len();
@@ -169,19 +192,11 @@ pub fn sign_tx(tx: ckb_types::core::TransactionView, key: &Privkey) -> Transacti
         .build()
 }
 
-pub fn gen_lockscript(lock_args: H160) -> packed::Script {
+// input data: lock_arg
+pub fn gen_lockscript() -> packed::Script {
     packed::Script::new_builder()
         .code_hash(SIGHASH_TYPE_HASH.pack())
         .hash_type(ScriptHashType::Data.into())
-        .args(Bytes::from(lock_args.as_bytes().to_vec()).pack())
+        .args(Bytes::from(h160!("0x4ddd5d32e1ee8bed83360fac2f70b03a6e4378b2").as_ref()).pack())
         .build()
-}
-
-pub fn gen_lock_args(privkey_key: H256) -> H160 {
-    let privkey = secp256k1::SecretKey::from_slice(privkey_key.as_bytes()).unwrap();
-    let pubkey = secp256k1::PublicKey::from_secret_key(&SECP256K1, &privkey);
-
-    let lock_arg = H160::from_slice(&blake2b_256(&pubkey.serialize()[..])[0..20])
-        .expect("Generate hash(H160) from pubkey failed");
-    lock_arg
 }
